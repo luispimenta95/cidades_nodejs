08cb90fa4e6f103142b69fa7de9fef8d6df01490
Luis Felipe <luisfelipearaujopimenta@gmail.com>
2023-07-15 15:59:20 -0300

    aruivos de log criados

--- main.js
+++ main.js
@@ -25,6 +25,14 @@ async function pesquisarCep() {
 }
 
 function escreverDados(dados) {
+    fs.writeFile(dados.bairro + "/response_" + dados.bairro + ".json", JSON.stringify(dados, null, 2), (err) => {
+        if (err) console.log(err);
+        else {
+            console.log("File written successfully\n");
+            console.log("The written has the following contents:");
+        }
+    });
+    //fs.writeFile(dados.bairro + "/tex.txt");
     createFolder(dados, dados.bairro + "/imagens");
 }
 
